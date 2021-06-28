1e1deccc5db3ed486eca8de55c2fcfc6d6986740
sandoya1 <83371887+sandoya1@users.noreply.github.com>
2021-06-28 13:44:53 -0500

    Create Funciones en cadena.py

---
+++ Funciones en cadena.py
@@ -0,0 +1,10 @@
+# Isaias Rafael Sandoya Vargas 
+# Software
+# #Tercer semestre
+
+# funciones de cadenas
+mensaje = 'Bienvenidos' + ' a mi nuevo ' + 'Programa de python'
+men1=mensaje.split()
+men2=' '.join(men1)
+print(mensaje[0],mensaje[0:4],men1,men2)
+print(mensaje.find("a mi nuevo"), mensaje.lower())
