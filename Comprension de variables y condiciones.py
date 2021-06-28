cc6dfa09843c77277e6ef81f63ac33507273cc41
sandoya1 <83371887+sandoya1@users.noreply.github.com>
2021-06-28 13:40:19 -0500

    Create Comprension de variables y condiciones.py

---
+++ Comprension de variables y condiciones.py
@@ -0,0 +1,19 @@
+# Isaias Rafael Sandoya Vargas 
+# Software
+# #Tercer semestre
+
+[car for car in['a','e','i','o','u'] if car not in('a','i','o')]
+edad,_peso = 20, 70.5
+nombres = 'Isaias Sandoya Vargas '
+dirDomiciliaria= "Jujan, avenida 16 de febrero"
+Tipo_sexo = 'M'
+civil = True
+usuario = ('isandyav','1234','isaias2019sandoya@gmail.com')
+materias = ['Programacion Web','PHP','POO']
+docente = {'nombre':'Isaias','edad':20}
+print("""Mi nombre es {}, tengo {} años""".format(nombres,edad))
+print(usuario,materias,docente)
+import math
+num1, num2, num, men = 12.572,  15.4,  4,  '1234'
+print(math.ceil(num1), '\t',math.floor(num1))
+print(round(num1,1),'\t',type(num),'\t',type(men))
