f5fce3db68fdeb8047c8bb2cf5d0facfc3302338
sandoya1 <83371887+sandoya1@users.noreply.github.com>
2021-06-28 13:41:25 -0500

    Create Ejemplo en clase.py

---
+++ Ejemplo en clase.py
@@ -0,0 +1,9 @@
+# Isaias Rafael Sandoya Vargas 
+# Software
+# #Tercer semestre
+
+# for range(v) – range(vi,vf)  - range(vi,vf,inc)
+frase = input("Ingrese frase: ")
+for indice in range(len(frase)):
+    print(indice,'=',frase[indice])
+    
