a35b46ecf25367f4f3a2f23a8c2c765faa585ebb
sandoya1 <83371887+sandoya1@users.noreply.github.com>
2021-06-28 13:43:29 -0500

    Create For Range.py

---
+++ For Range.py
@@ -0,0 +1,8 @@
+# Isaias Rafael Sandoya Vargas 
+# Software
+# #Tercer semestre
+
+# for range(v) – range(vi,vf)  - range(vi,vf,inc)
+frase = input("Ingrese frase a usar: ")
+for indice in range(len(frase)):
+    print(indice,'=',frase[indice])
